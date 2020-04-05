05d9b41c81a832f96e10ad62a3b44e34ef4d7fa6
Dimitra Karamperi <dkaramperi@yahoo.gr>
2020-04-05 15:20:41 +0300

    uncomment grades section

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ if (this.state.buttonClicked === "students") {
     }
 
     /* Uncomment lines below to render grades*/
-    /*if (this.state.buttonClicked === "grades") {
+  if (this.state.buttonClicked === "grades") {
       tabChoice = (
         <Table
           tableNames={this.state.assignments}
@@ -89,7 +89,7 @@ if (this.state.buttonClicked === "students") {
           data={this.state.grades}
         />
       );
-    }*/
+    }
 
     return (
       <div>
